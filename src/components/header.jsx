b78b1e5727cbf8f4bed3f0ab99f80a7c3dfb6d4c
taherahmed14 <taherahmed.sj.95@gmail.com>
2022-03-08 00:25:49 +0530

    debounce

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import "./header.css";
+import { SearchBox } from "./searchBox";
 
 export const Navbar = () => {
     return(
         <div className="navbar">
             <Link to={"/"} className="logo">Wines</Link>
             <div>
+                <SearchBox />
                 <Link to={"/cart"} className="links">Your Cart</Link>
                 <Link to={"/wishlist"} className="links">Wishlist</Link>
             </div>
---
+++ src/components/searchBox.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import "./searchBox.css";
+
+function useDebounce(value, delay) {
+    console.log("Debouncing");
+    const [debounceValue, setDebounceValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebounceValue(value);
+        }, delay);
+        //clean up function
+        return () => {
+            clearTimeout(handler);
+        }  
+    }, [value, delay]);
+    return debounceValue;
+}
+
+export const SearchBox = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const [results, setResults] = useState([]);
+    const [allProducts, setAllProducts] = useState([]);
+
+    const debounce = useDebounce(searchInput, 500);
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    const getProducts = async () => {
+        const baseURL = 'https://api.sampleapis.com/wines/reds';
+        fetch(baseURL)
+        .then(resp => resp.json())
+        .then(data => setAllProducts(data.slice(0, 100)));
+    };
+
+    useEffect(() => {
+        console.log(debounce);
+        if(debounce) {
+            setResults(
+                allProducts.filter(product => product.winery.toLowerCase().includes(searchInput.toLowerCase()))
+            );
+        }
+        else {
+            setResults([]);
+        }
+    }, [debounce]);
+
+    return (
+        <div className="searchContainer">
+            <input className="searchInput" placeholder="Search" onChange={e => setSearchInput(e.target.value)} />
+
+            <div className="results">
+                {results?.map(product => 
+                    <div>{product.winery}</div>)
+                }
+            </div>
+        </div>
+    )
+};
